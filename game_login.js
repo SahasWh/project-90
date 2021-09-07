a49082f19f6f2ea05fd9a2bf259bcb257d8cfad4
SahasWh <82063178+SahasWh@users.noreply.github.com>
2021-09-07 09:22:02 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function add_user() {
+    player1_name = document.getElementById("player_1_name").Value;
+    player2_name = document.getElementById("player_1_name").Value;
+
+    localStorage.setItem("player_1_name",player1_name);
+    localStorage.setItem("player_1_name",player1_name);
+
+    window.location.replace("game_page.html");
+}
